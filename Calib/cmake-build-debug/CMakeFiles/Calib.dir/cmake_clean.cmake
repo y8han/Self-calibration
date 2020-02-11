fc2a3e9f2e8382d73f23d4aaa7257dd45e67ba19
y8han <y8han@ucsd.edu>
2020-02-11 13:53:50 -0800

    chessboard_calibration

---
+++ Calib/cmake-build-debug/CMakeFiles/Calib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calib.dir/main.cpp.o"
+  "Calib.pdb"
+  "Calib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Calib/cmake-build-debug/CMakeFiles/Calib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Calib.
+# This may be replaced when dependencies are built.
